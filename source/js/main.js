5c5522c26ad6d5d847044039b53a66be981d16b0
Kirill Gadomskii <gadomskyka@mail.ru>
2021-01-16 18:04:46 +0400

    Refactor features section, js, header block

--- source/js/main.js
+++ source/js/main.js
@@ -12,7 +12,6 @@ navToggle.classList.remove("main-nav__toggle--disabled");
 navList.classList.add("main-nav__list--closed");
 
 navToggle.addEventListener("click", function (event) {
-  event.preventDefault();
   header.classList.toggle("header--open-menu");
   headerInner.classList.toggle("header__inner--open-menu");
   headerLogo.classList.toggle("header__logo--open-menu");
